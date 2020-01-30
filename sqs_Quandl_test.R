9b8c7c15eaaca968802b4ab196c7753ead6e7e72
Renaud Gironne <gironner@skemalab.io>
2020-01-30 15:28:02 +0000

    Delete test

--- sqs_Quandl_test.R
+++ sqs_Quandl_test.R
@@ -1,6 +1,8 @@
 library(Quandl)
 Quandl.api_key("dgHj2ix2qxWUKwEgWvR9")
 
-sqs_Quandl_
+sqs_Quandl_data <- function(term, start, end) {
+  return(Quandl(term, start_date=start, end_date=end))
+}
 
-Test
+sqs_Quandl_data("FRED/GDP", "2001-12-31", "2005-12-31")
